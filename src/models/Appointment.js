bfbb6257698d13687ce2a682a07815b8bfa8436a
Damian Palavecino <damianpalavecino25@gmail.com>
2023-02-06 19:50:49 -0300

    feat: add hour and expired properties

--- src/models/Appointment.js
+++ src/models/Appointment.js
@@ -4,6 +4,8 @@ const appointmentsSchema = new Schema({
   serviceProviders: { type: Array, required: true },
   client: { type: String, required: true },
   day: { type: Date, required: true },
+  hour: { type: String, required: true },
+  expired: { type: Boolean },
 });
 
 export default mongoose.model('Appointments', appointmentsSchema);
