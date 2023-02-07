9e70e657cb59ad9725f588d3b87214179bf094c9
Ernesto Bessone <ejubessone@gmail.com>
2023-02-07 16:12:10 -0300

    feat: added users route on index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import usersRouter from './users.js';
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
 });
+router.use("/users", usersRouter);
 
 export default router;
