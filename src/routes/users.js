b144f357d80aae374aa6e887f71cf9337667d0cb
Ernesto Bessone <ejubessone@gmail.com>
2023-02-07 16:24:34 -0300

    feat: delete user route added

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,8 @@ import express from "express";
 import {
     getUsers,
     getUserById,
-    createUser
+    createUser,
+    deleteUser
 } from '../controllers/user.js';
 
 const router = express.Router();
@@ -11,6 +12,7 @@ const router = express.Router();
 router
     .get('/', getUsers)
     .get('/:id', getUserById)
-    .post('/', createUser);
+    .post('/', createUser)
+    .delete('/:id', deleteUser);
 
 export default router;
