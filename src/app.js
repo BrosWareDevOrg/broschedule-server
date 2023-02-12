9781042b90b0a430445af4576fe2a4d39cfd674f
Damian Palavecino <damianpalavecino25@gmail.com>
2023-02-12 20:09:36 -0300

    remove comments

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import cors from 'cors';
-// import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/index.js';
@@ -9,5 +8,4 @@ dotenv.config();
 export const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(express.static(path.join('src', 'public')))
 app.use('/api', router);
