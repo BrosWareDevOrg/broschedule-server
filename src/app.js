acc5376eb64febae0c9c5e9c1113f0684fbfe71d
Damian Palavecino <damianpalavecino25@gmail.com>
2023-02-12 20:06:13 -0300

    remove index html

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import cors from "cors";
-import path from 'path';
-import express from "express";
+import cors from 'cors';
+// import path from 'path';
+import express from 'express';
 import dotenv from 'dotenv';
-import router from "./routes/index.js";
+import router from './routes/index.js';
 
 dotenv.config();
 
 export const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join('src', 'public')))
+// app.use(express.static(path.join('src', 'public')))
 app.use('/api', router);
