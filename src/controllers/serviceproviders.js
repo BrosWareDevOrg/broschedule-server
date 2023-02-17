f4266590d5a30a90cf4e16bae4584e983e3578a5
Julian Flores <julianfloresdev@gmail.com>
2023-02-17 11:13:35 -0300

    fix: serviceProvider typo / prev user exist validation

--- src/controllers/serviceproviders.js
+++ src/controllers/serviceproviders.js
@@ -5,12 +5,15 @@ import firebase from '../helpers/firebase/config.js';
 
 export const createServiceProvider = async (req, res) => {
   try {
-    const userExist = await ServiceProviders.find(req.body.email) || Users.find(req.body.email);
-    if (userExist) {
+    const providerExist = await ServiceProviders.findOne({
+      email: req.body.email,
+    });
+    const userExist = await Users.findOne({ email: req.body.email });
+    if (providerExist || userExist) {
       return res.status(451).json({
         message: 'This email has already been registered',
         error: true,
-        data: req.body,
+        data: providerExist || userExist,
       });
     }
 
--- src/models/ServiceProviders.js
+++ src/models/ServiceProviders.js
@@ -17,7 +17,12 @@ const serviceProviderSchema = new Schema(
     //Define an array of avaible days for work, default: [1, 2, 3, 4, 5] define an array of days between weekends day.
     availableDays: {
       type: Array,
-      required: false,
+      required: true,
+      default: [1, 2, 3, 4, 5],
+      items: {
+        type: Number,
+        required: false,
+      },
     },
     //If service provider work in fisical location, it must be declareted here, otherwise this field is allow to be empty
     location: {
