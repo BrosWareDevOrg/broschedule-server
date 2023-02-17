a3f8bf63ace1d9f7001c5c9098c45c030ce16f1d
Julian Flores <julianfloresdev@gmail.com>
2023-02-17 11:17:35 -0300

    feat: add firebaseUid on User Schema

--- src/models/User.js
+++ src/models/User.js
@@ -2,20 +2,27 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  phone: { type: String, required: true },
-  email: { type: String, required: true },
-  isActive: { type: Boolean, default: true },
-  appointments: [
-    {
-      _id: false,
-      type: Schema.Types.ObjectId,
-      ref: 'Appointments',
-      required: false,
-    }
-  ],
-}, { timestamps: true });
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phone: { type: String, required: true },
+    email: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+    appointments: [
+      {
+        _id: false,
+        type: Schema.Types.ObjectId,
+        ref: 'Appointments',
+        required: false,
+      },
+    ],
+    firebaseUid: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model('Users', userSchema);
